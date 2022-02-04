bd88d34078cef4703f356d77c332bd3ff3f4538d
cchalc <cchalc>
2022-02-04 06:24:07 +0000

    added a setup notebook

---
+++ wwxf/resources/setup.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %md 
+# MAGIC # Set up configuration
+# MAGIC ## Libraries
+
+# COMMAND ----------
+
+# MAGIC %pip install /dbfs/databricks/libraries/pyrasterframes-0.10.1.dev0+dbr7.3-py3-none-any.whl
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Path and database configuration
